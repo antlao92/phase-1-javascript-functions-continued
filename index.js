fd80f1608021aa72270f898e7a814a007accf8e8
Anthony <anthonykimlao@gmail.com>
2023-12-12 02:57:52 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(){
+    return "This Saturday, I want to"
+}
+function saturdayFun(activity= "roller-skate"){
+return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office"){
+
+   return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(wrapper="*"){
+    const innerFunction = function(adjective = "a hard worker"){ 
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    }
+    return innerFunction
+}
+wrapAdjective("||")("a dedicated programmer");
